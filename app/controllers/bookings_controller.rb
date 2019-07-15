4c5d126823c4fe7312bfac144ff4f606ad8b2247
Bart Karnath <karnath.bart@gmail.com>
2019-07-15 11:20:12 +0100

    working on bookings to be assigned to a user:

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,12 @@
 class BookingsController < ApplicationController
+  def index
+    @user = current_user
+    @bookings = Booking.where(user: @user)
+    @games = Game.where(user: @user)
+  end
+
+  def show
+    @user = current_user
+    @booking = Booking.find(params[:id])
+  end
 end
